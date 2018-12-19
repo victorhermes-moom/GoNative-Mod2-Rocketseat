1be8a37736b432528fee0ee546da075256d51897
Victor Hermes <me.victorhermes@gmail.com>
2018-12-19 21:25:51 -0200

    Reactotron

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./config/ReactotronConfig";
+import React from "react";
+
+import { View, Text } from "react-native";
+
+const App = () => (
+    <View>
+        <Text>asdsad</Text>
+    </View>
+);
+export default App;
