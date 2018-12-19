a7334284cc378804cfd18a2f946037ebbcb5085d
Victor Hermes <me.victorhermes@gmail.com>
2018-12-19 21:56:30 -0200

    Estrutura criada

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,5 +1,7 @@
 import Reactotron from "reactotron-react-native";
 
-Reactotron.configure() // controls connection & communication settings
+Reactotron.configure({
+    host: "192.168.0.104"
+}) // controls connection & communication settings
     .useReactNative() // add all built-in react native plugins
     .connect(); // let's connect!
